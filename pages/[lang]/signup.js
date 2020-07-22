812180e51bcb8ccb8102b87687b8f0627b9c49a1
saliha54 <51030929+saliha54@users.noreply.github.com>
2020-07-23 00:25:43 +0200

    signup update with styling + 1/2 validation

--- pages/[lang]/signup.js
+++ pages/[lang]/signup.js
@@ -5,6 +5,10 @@ import Layout from "../../components/Layout";
 import WithLocaleWrapper from "../../hocs/withLocale";
 import useTranslation from "../../hooks/useTranslation";
 import fetch from "node-fetch";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { validateEmail, validatePassword } from '../../utils/validation'
+
 const urlEndpoint = `https://fitigai-api.herokuapp.com/v1/`;
 
 const Signup = () => {
@@ -19,81 +23,79 @@ const Signup = () => {
 
   async function handleSubmit(e) {
     e.preventDefault();
-    const body = { email, password, firstName, lastName, phone };
-    const response = await fetch(`${urlEndpoint}auth/register-local`, {
-      method: "post",
-      body: JSON.stringify(body),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      const cookieToken = cookie.get("token");
-      cookie.set("token", cookieToken, { expires: 2 });
-      Router.push("/");
+    if (!validateEmail(email))
+      console.log('Email is Invalid')
+    else if (!validatePassword(password))
+      console.log('Password is Invalid, must be at least 6 characters')
+    else if (firstName == '' | lastName == '')
+      console.log('Name-LastName Missing')
+    {
+      const body = { email, password, firstName, lastName, phone };
+      const response = await fetch(`${urlEndpoint}auth/register-local`, {
+        method: "post",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" },
+      });
+      if (response.ok) {
+        const cookieToken = cookie.get("token");
+        cookie.set("token", cookieToken, { expires: 2 });
+        Router.push("/");
+      }
     }
   }
   return (
     <Layout>
-      <form onSubmit={handleSubmit}>
-        <p>{t("Signup")}</p>
-        <label htmlFor="email">
-          {t("Email")}
-          <input
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            name="email"
-            type="email"
-            placeholder="email"
-          />
-        </label>
-
-        <label htmlFor="password">
-          {t("Password")}
-          <input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-            type="password"
-            placeholder="password"
-          />
-        </label>
-
-        <label htmlFor="firstName">
-          {t("FirstName")}
-          <input
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            name="firstName"
-            type="firstName"
-            placeholder="firstName"
-          />
-        </label>
-
-        <label htmlFor="lastName">
-          {t("LastName")}
-          <input
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            name="lastName"
-            type="lastName"
-            placeholder="lastName"
-          />
-        </label>
-
-        <label htmlFor="phone">
-          {t("Phone")}
-          <input
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            name="phone"
-            type="phone"
-            placeholder="phone"
-          />
-        </label>
-
-        <button type="submit">{t("Signup")}</button>
+      <form style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%', alignItems: 'center', justifyContent: 'space-evenly' }}>
+        <p>{t('Signup')}</p>
+        <TextField
+          id="outlined-required"
+          name="firstName"
+          type="firstName"
+          variant="outlined"
+          placeholder={t('FirstName')}
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <TextField
+          id="outlined-required"
+          name="lastName"
+          type="lastName"
+          variant="outlined"
+          placeholder={t('LastName')}
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <TextField
+          id="outlined-required"
+          name="email"
+          type="email"
+          variant="outlined"
+          placeholder={t('Email')}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          id="outlined-number"
+          name="phone"
+          type="phone"
+          variant="outlined"
+          placeholder={t('Phone')}
+          value={phone}
+          onChange={(e) => setPhone(e.target.value.replace(/[^\d]/, ''))}
+        />
+        <TextField
+          id="outlined-required"
+          name="password"
+          type="password"
+          variant="outlined"
+          placeholder={t("Password")}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button variant="contained" value={"Signup"} onClick={(e) => handleSubmit(e)} >{t("Signup")}</Button>
         {signupError && <p style={{ color: "red" }}>{signupError}</p>}
       </form>
-    </Layout>
+    </Layout >
   );
 };
 
