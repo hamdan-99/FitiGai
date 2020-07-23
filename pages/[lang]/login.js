171bef195be658d9ae582fa4464d64ecafd94de4
saliha54 <51030929+saliha54@users.noreply.github.com>
2020-07-23 11:54:00 +0200

    newest update login.js

--- pages/[lang]/login.js
+++ pages/[lang]/login.js
@@ -4,6 +4,9 @@ import cookie from "js-cookie";
 import Layout from "../../components/Layout";
 import WithLocaleWrapper from "../../hocs/withLocale";
 import useTranslation from "../../hooks/useTranslation";
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { validateEmail, validatePassword } from '../../utils/validation'
 
 const urlEndpoint = `https://fitigai-api.herokuapp.com/v1/`;
 
@@ -33,24 +36,35 @@ const Login = (props) => {
           Router.push("/");
         }
       });
+
   }
   return (
     <Layout>
-      <form onSubmit={handleSubmit}>
-        <p>{t("Login")}</p>
-        <input
+      <form style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '60%', alignItems: 'center', justifyContent: 'space-evenly' }}>
+        <p>{t('Login')}</p>
+        <TextField
+          required
+          label={t('Email')}
+          id="outlined-required"
           name="email"
           type="email"
+          variant="outlined"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input
+        <TextField
+          required
+          label={t("Password")}
+          // 
+          id="outlined-required"
           name="password"
           type="password"
+          variant="outlined"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          helperText={(password === '' || validatePassword(password)) ? "" : 'At least 6 characters'}
         />
-        <input type="submit" value={t("Submit")} />
+        <Button variant="contained" value={"Login"} disabled={!(validateEmail(email) && validatePassword(password))} onClick={(e) => handleSubmit(e)} >{t("Login")}</Button>
         {loginError && <p style={{ color: "red" }}>{loginError}</p>}
       </form>
     </Layout>
