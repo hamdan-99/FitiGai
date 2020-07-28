e9c0a9551af2534596718ac34828c0e3702961b5
Saliha <salihayaylaci@g.ucla.edu>
2020-07-28 19:45:00 +0200

    advanced search fix

--- components/advancedSearch.js
+++ components/advancedSearch.js
@@ -1,97 +1,80 @@
-import React, { Component } from "react";
+
+import React, { useState, useEffect } from "react";
 import _ from "lodash";
 
-class AdvancedSearch extends Component {
-  state = {
-    services: [],
-    coaches: [],
-    sport: [],
-    language: [],
-    location: [],
-    minPrice: [],
-    maxPrice: [],
-    result: [],
-    error: [],
-  };
-  componentDidMount() {
-    this.setState({ services: this.props.services });
-    this.setState({ coaches: this.props.coaches });
-  }
-  componentDidUpdate(prevState) {
-    // if (this.state.result !== 0) {
-    //   if (prevState.result !== this.state.result) {
-    //     this.props.result.as = [...this.state.result];
-    //   }else{
-    //     return null;
-    //   }
-    //   console.log(
-    //     "parent",
-    //     this.props.result,
-    //     "result",
-    //     this.state.result,
-    //     "result",
-    //     this.state.sport
-    //   );
-    // } else {
-    //   return null;
-    // }
-  }
+let AdvancedSearch = (props) => {
 
-  handleChangeSport = (e) => {
+  let [services, setServices] = useState(props.services)
+  let [coaches, setCoaches] = useState(props.coaches)
+  let [sport, setSport] = useState([])
+  let [language, setLanguage] = useState([])
+  let [location, setLocation] = useState([])
+  let [minPrice, setMinPrice] = useState([])
+  let [maxPrice, setMaxPrice] = useState([])
+  let [result, setResult] = useState([])
+  let [error, setError] = useState([])
+
+
+
+
+  let handleChangeSport = (e) => {
     e.preventDefault();
     const sport = [];
     const sportError = [];
-    this.state.services.map((i, index) => {
+    services.map((i, index) => {
       if (i.title.toLowerCase() === e.target.value.toLowerCase()) {
         sport.push(i);
       } else if (i.title.toLowerCase() !== e.target.value.toLowerCase()) {
         sportError.push(` ${e.target.value} is not supported Sport`);
-      } else {
-        return null;
       }
     });
-    this.setState({ sport: sport });
-    this.state.error.push(sportError[0]);
+    setSport(sport);
+    setError([...error, sportError[0]])
+
+    console.log('sport', sport)
   };
 
-  handleChangeLanguage = (e) => {
+  let handleChangeLanguage = (e) => {
     e.preventDefault();
     // this is result for match coach sport title and langugage supported
     const language = [];
-    this.state.coaches.map((i) => {
-      if (i.lang.slice(0, 2) === e.target.value) {
+    coaches.map((i) => {
+      if (e.target.value === 'All Languages')
         language.push(i._id);
-      } else {
-        return null;
-      }
+      else
+        if (i.lang.slice(0, 2) === e.target.value) {
+          language.push(i._id);
+        }
     });
-    this.setState({ language: language });
+    setLanguage(language);
+
+    console.log('language', language)
   };
 
-  handleChangeLocation = (e) => {
+  let handleChangeLocation = (e) => {
     e.preventDefault();
     const location = [];
     const locationError = [];
-    this.state.services.map((i) => {
+    services.map((i) => {
       if (i.location.toLowerCase() === e.target.value.toLowerCase()) {
         location.pop();
         location.push(e.target.value.toLowerCase());
       } else if (i.location.toLowerCase() !== e.target.value.toLowerCase()) {
         locationError.pop();
         locationError.push(` ${e.target.value} is not supported location`);
-      } else {
-        return null;
       }
     });
-    this.setState({ location: location });
-    this.state.error.push(locationError[0]);
+    setLocation(location);
+    setError([...error, locationError[0]])
+
+    console.log('location', location)
   };
 
-  handleChangePriceMin = (e) => {
+  let handleChangePriceMin = (e) => {
     e.preventDefault();
     const minPrice = [];
     const minError = [];
-    const number = this.state.services.map((i) => i.price);
+    const number = services.map((i) => i.price);
     const min = Math.min(...number);
     const max = Math.max(...number);
 
@@ -100,18 +83,19 @@ class AdvancedSearch extends Component {
       minPrice.push(e.target.value);
     } else if (!_.inRange(e.target.value, min, max)) {
       return [minError.push(`there is no less than min ${e.target.value}`)];
-    } else {
-      return null;
     }
-    this.setState({ minPrice: minPrice });
-    this.state.error.push(minError[0]);
+    setMinPrice(minPrice);
+    setError([...error, minError[0]])
+
+    console.log('minP', minPrice)
+
   };
 
-  handleChangePriceMax = (e) => {
+  let handleChangePriceMax = (e) => {
     e.preventDefault();
     const maxPrice = [];
     const maxError = [];
-    const number = this.state.services.map((i) => i.price);
+    const number = services.map((i) => i.price);
     const min = Math.min(...number);
     const max = Math.max(...number);
 
@@ -120,157 +104,154 @@ class AdvancedSearch extends Component {
       maxPrice.push(e.target.value);
     } else if (!_.inRange(e.target.value, min, max)) {
       maxError.push(`there is no greater than  ${e.target.value}`);
-    } else {
-      return null;
     }
-    this.setState({ maxPrice: maxPrice });
-    this.state.error.push(maxError[0]);
+    setMaxPrice(maxPrice);
+    setError([...error, maxError[0]])
+
+    console.log('maxP', maxPrice)
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    // this is result of sport and language and location
+  useEffect(() => {
     const results = [];
-
-    const { sport, language, location, maxPrice, minPrice, error } = this.state;
     sport.map((service) =>
       language.map((lang) =>
         location.map((loc) => {
           if (service.location.toLowerCase() === loc) {
             if (service.owner === lang) {
               results.push(service);
-              this.setState({ result: results });
-            } else {
-              return null;
+              setResult(results);
             }
-          } else {
-            return null;
           }
         })
       )
     );
+
     if (results.length === 0) {
       if (language.length === 0) {
         if (location.length === 0) {
           const values = sport.values();
           for (const value of values) {
             results.push(value);
-            this.setState({ result: results });
+            setResult(results);
           }
         } else if (location.length !== 0) {
           sport.map((i) =>
             location.map((l) => {
               if (i.location.toLowerCase() === l) {
                 results.push(i);
-                this.setState({ result: results });
-              } else {
-                return null;
+                setResult(results);
               }
             })
           );
-        } else {
-          return null;
         }
       } else if (language.length !== 0) {
         sport.map((i) =>
           language.map((l) => {
             if (i.owner === l) {
               results.push(i);
-              this.setState({ result: results });
-            } else {
-              return null;
+              setResult(results);
             }
           })
         );
-      } else {
-        return null;
       }
-    } else {
-      return null;
     }
+
+
     const priceResult = [];
     if (results.length !== 0) {
       if (minPrice.length !== 0 && maxPrice.length !== 0) {
         results.map((i) => {
           if (_.inRange(i.price, minPrice, maxPrice)) {
             priceResult.push(i);
-            this.setState({ result: priceResult });
+            setResult(priceResult);
           } else {
             return null;
           }
         });
-      } else {
-        return null;
       }
-    } else {
-      return null;
     }
+
+    console.log('resutl effect', result)
+
+  }, [sport, language, location, minPrice, maxPrice])
+
+  let handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('clicked', result)
+    props.setResults([...result])
+    // this is result of sport and language and location
   };
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <div className="sport">
-            <input onChange={this.handleChangeSport} placeholder="Sport" />
-            {/* List Of Sports we could use */}
-            {/* tennis      */}
-            {/* Volleyball  */}
-            {/* Football    */}
-            {/* Badminton   */}
-            {/* Disability  */}
-            {/* Diving      */}
-            {/* Boxing      */}
-            {/* Judo        */}
-            {/* Swimming    */}
-            {/* Table Tennis*/}
-            {/* Taekwondo   */}
-            {/* Wrestling   */}
-          </div>
 
-          <div className="language">
-            <select onChange={this.handleChangeLanguage}>
-              <option defaultValue>Choose Language</option>
-              <option value="en">English</option>
-              <option value="fr">French</option>
-            </select>
+  return (
+    <div>
+      <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-around', paddingTop: '8vh' }}>
+        <div className="sport">
+          <input onChange={handleChangeSport} placeholder="Sport" className='form-control' defaultValue={props.initialProps[0]} />
+          {/* List Of Sports we could use */}
+          {/* tennis      */}
+          {/* Volleyball  */}
+          {/* Football    */}
+          {/* Badminton   */}
+          {/* Disability  */}
+          {/* Diving      */}
+          {/* Boxing      */}
+          {/* Judo        */}
+          {/* Swimming    */}
+          {/* Table Tennis*/}
+          {/* Taekwondo   */}
+          {/* Wrestling   */}
+        </div>
+        <div className="location">
+          <input
+            onChange={handleChangeLocation}
+            placeholder="Location"
+            className='form-control'
+            defaultValue={props.initialProps[1]}
+          />
+          {/*Languages   */}
+          {/* brussels */}
+          {/*  Leuven  */}
+          {/*   Paris  */}
+        </div>
+        <div className="language">
+          <select onChange={handleChangeLanguage} style={{
+            border: '4px', borderColor: 'black', borderRadius: '5px', width: '150px', height: '40px', textAlign: 'left', textAlignLast: 'left', backgroundColor: 'white', paddingLeft: '5px'
+          }}>
+            <option defaultValue>All Languages</option>
+            <option value="en">English</option>
+            <option value="fr">French</option>
+          </select>
 
-            {/*Languages  */}
-            {/* Dutch  */}
-            {/* French */}
-            {/* English*/}
-          </div>
+          {/*Languages  */}
+          {/* Dutch  */}
+          {/* French */}
+          {/* English*/}
+        </div>
+        <div className="priceMin">
+          <input
+            onChange={handleChangePriceMin}
+            placeholder="Min Price"
+            type="number"
+            className='form-control'
+            style={{ width: '150px' }}
+          />
+        </div>
 
-          <div className="location">
-            <input
-              onChange={this.handleChangeLocation}
-              placeholder="location"
-            />
-            {/*Languages   */}
-            {/* brussels */}
-            {/*  Leuven  */}
-            {/*   Paris  */}
-          </div>
-
-          <div className="priceMin">
-            <input
-              onChange={this.handleChangePriceMin}
-              placeholder="Min Price"
-              type="number"
-            />
-          </div>
-
-          <div className="priceMax">
-            <input
-              onChange={this.handleChangePriceMax}
-              placeholder="Max Price"
-              type="number"
-            />
-          </div>
-          <button>Search</button>
-        </form>
-      </div>
-    );
-  }
+        <div className="priceMax">
+          <input
+            onChange={handleChangePriceMax}
+            placeholder="Max Price"
+            type="number"
+            className='form-control'
+            style={{ width: '150px' }}
+          />
+        </div>
+        <button style={{
+          border: '0', borderRadius: '20px', width: '100px', height: '40px', textAlign: 'center', textAlignLast: 'center', color: 'white', backgroundColor: 'rgb(65,161,242)'
+        }}>Search</button>
+      </form>
+    </div>
+  );
 }
 
 export default AdvancedSearch;
--- pages/[lang]/results.js
+++ pages/[lang]/results.js
@@ -1,3 +1,204 @@
+// import Card from '../../components/Cards';
+// import { Grid } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Layout from '../../components/Layout';
+// import Pagination from '../../components/pagination';
+// import { useState, useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import WithLocaleWrapper from '../../hocs/withLocale';
+// import useTranslation from '../../hooks/useTranslation';
+// import AdvancedSearch from '../../components/advancedSearch';
+
+// const urlEndpoint = `https://fitigai-api.herokuapp.com/v1/`;
+
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   gridContainer: {
+//     marginTop: '3rem',
+//     paddingLeft: '40px',
+//     paddingRight: '40px',
+//   },
+// }));
+
+// const Results = ({ services, coaches, title, location }) => {
+//   const router = useRouter();
+//   const { locale, t } = useTranslation();
+//   const classes = useStyles();
+
+//   let [number, setNumber] = useState(services.map((i) => i.price))
+//   let [a_title, setTitle] = useState(title)
+//   let [a_location, setLocation] = useState(location)
+//   let [a_language, setLanguage] = useState(['EN', 'FR'])
+//   let [a_maxPrice, setMaxPrice] = useState(Math.max(...number))
+//   let [a_minPrice, setMinPrice] = useState(Math.min(...number))
+//   let [clicked, setClicked] = useState(0)
+
+//   let [results, setResults] = useState(
+//     services.filter((i, e) => {
+//       if (i.title.toLowerCase().includes(a_title.toLowerCase())) {
+//         if (i.location.toLowerCase().includes(a_location.toLowerCase())) {
+//           let coach = coaches.find(x => x._id === i.owner)
+//           if (coach) {
+//             if (a_language.includes(coach.lang.slice(0, 2).toUpperCase())) {
+//               if (i.price >= a_minPrice && i.price <= a_maxPrice)
+//                 return i;
+//             }
+//           }
+//         }
+//       }
+//       return null;
+//     })
+//   );
+
+
+//   useEffect(() => {
+//     setResults(services.filter((i, e) => {
+//       if (i.title.toLowerCase().includes(a_title.toLowerCase())) {
+//         if (i.location.toLowerCase().includes(a_location.toLowerCase())) {
+//           let coach = coaches.find(x => x._id === i.owner)
+//           if (coach) {
+//             if (a_language.includes(coach.lang.slice(0, 2).toUpperCase())) {
+//               if (i.price >= a_minPrice && i.price <= a_maxPrice)
+//                 return i;
+//             }
+//           }
+//         }
+//       }
+//       return null;
+//     }))
+//   }, [clicked])
+
+
+
+//   const b = [];
+//   let [a, setA] = useState({ as: [] });
+//   if (a.as.length >= 0) {
+//     a.as.map((i) => b.push(i));
+//   } else {
+//     return null;
+//   }
+
+//   useEffect(() => {
+//     if (results.length === 0)
+//       setResults(services.map((i, e) => (e < 32 ? i : null)));
+//   }, [results]);
+
+//   let [pageSize, SetPageSize] = useState(9);
+//   let [currentPage, SetCurrentPage] = useState(1);
+
+//   useEffect(() => {
+//     SetCurrentPage(1);
+//   }, [results]);
+
+//   const handlePageChange = (page) => {
+//     SetCurrentPage(page);
+//   };
+
+//   const handleClick = (coachID, serviceID) => {
+//     router.push({
+//       pathname: `/${locale}/profile`,
+//       query: {
+//         _id: serviceID._id,
+//         location: serviceID.location,
+//         owner: serviceID.owner,
+//         title: serviceID.title,
+//         description: serviceID.description,
+//         price: serviceID.price,
+//         address: serviceID.address,
+//         lang: coachID.lang,
+//         firstName: coachID.firstName,
+//         lastName: coachID.lastName,
+//       },
+//     });
+//   };
+
+//   return (
+//     <Layout>
+//       <div className='wrapper'>
+//         <div className='container'>
+//           <AdvancedSearch
+//             clicked={clicked}
+//             setClicked={setClicked}
+//             title={a_title}
+//             setTitle={setTitle}
+//             location={a_location}
+//             setLocation={setLocation}
+//             language={a_language}
+//             setLanguage={setLanguage}
+//             max={a_maxPrice}
+//             setMax={setMaxPrice}
+//             min={a_minPrice}
+//             setMin={setMinPrice}
+//           />
+
+//           <Grid
+//             container
+//             spacing={5}
+//             justify='center'
+//             className={classes.gridContainer}
+//           >
+//             {results
+//               .slice((currentPage - 1) * pageSize, currentPage * pageSize)
+//               .map((card) =>
+//                 coaches.map((coach) =>
+//                   card.owner === coach._id ? (
+//                     <Grid
+//                       key={card._id}
+//                       onClick={() => handleClick(coach, card)}
+//                       item
+//                       xs={12}
+//                       sm={6}
+//                       md={4}
+//                     >
+//                       <Card card={card} coachName={coach} key={card._id} />
+//                     </Grid>
+//                   ) : null
+//                 )
+//               )}
+//           </Grid>
+
+//           <div className='pagination'>
+//             <Pagination
+//               className='pagination'
+//               itemsCount={results.length}
+//               pageSize={pageSize}
+//               onPageChange={handlePageChange}
+//               currentPage={currentPage}
+//             />
+//           </div>
+//         </div>
+//         <style jsx>
+//           {`
+//             .wrapper {
+//               background-color: aliceblue;
+//             }
+//           `}
+//         </style>
+//       </div>
+//     </Layout>
+//   );
+// };
+
+// Results.getInitialProps = async ({ query }) => {
+//   const coaches_res = await fetch(`${urlEndpoint}coach/coaches`);
+//   const coaches = await coaches_res.json();
+//   const services_res = await fetch(`${urlEndpoint}searches`);
+//   const services = await services_res.json();
+
+//   return {
+//     coaches,
+//     services,
+//     title: query.title,
+//     location: query.location,
+//   };
+// };
+
+// export default WithLocaleWrapper(Results);
+
+
+
 import Card from '../../components/Cards';
 import { Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -16,7 +217,7 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
   gridContainer: {
-    marginTop: '3rem',
+    marginTop: '5vh',
     paddingLeft: '40px',
     paddingRight: '40px',
   },
@@ -35,34 +236,18 @@ const Results = ({ services, coaches, title, location }) => {
         else return null;
     })
   );
-  const b = [];
-  let [a, setA] = useState({ as: [] });
-  if (a.as.length >= 0) {
-    a.as.map((i) => b.push(i));
-  } else {
-    return null;
-  }
-  // useEffect(() => {
-  //   if (b.length !== 0 && b.length !== results.length) {
-  //     setResults(b);
-  //   } else {
-  //     return;
-  //   }
-  // }, [results.length]);
 
-  console.log("set", a.as, "Dad results", results);
+  let [pageSize, SetPageSize] = useState(12);
+  let [currentPage, SetCurrentPage] = useState(1);
+
 
   useEffect(() => {
+    console.log('PASSED RESULTS', results)
+    SetCurrentPage(1);
     if (results.length === 0)
       setResults(services.map((i, e) => (e < 32 ? i : null)));
   }, [results]);
 
-  let [pageSize, SetPageSize] = useState(9);
-  let [currentPage, SetCurrentPage] = useState(1);
-
-  useEffect(() => {
-    SetCurrentPage(1);
-  }, [results]);
 
   const handlePageChange = (page) => {
     SetCurrentPage(page);
@@ -94,7 +279,8 @@ const Results = ({ services, coaches, title, location }) => {
             services={services}
             coaches={coaches}
             className={classes.advance}
-            result={a.as}
+            setResults={setResults}
+            initialProps={[title, location]}
           />
 
           <Grid
